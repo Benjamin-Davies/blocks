1b720fcacc46b4399408e7ac4ace86e07ac2e101
Benjamin Davies <bentendavies@gmail.com>
2025-02-11 21:13:38 +1300

    refactor: split common code into crate

---
+++ blocks/src/main.rs
@@ -0,0 +1,13 @@
+use winit::{event_loop::EventLoop, window::WindowBuilder};
+
+use blocks::State;
+
+#[pollster::main]
+async fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new(&window, wgpu::Backends::GL).await;
+
+    state.run(event_loop).unwrap();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import init from 'blocks-web';
 
-// const canvas = document.createElement('canvas');
-// document.body.appendChild(canvas);
-
 init();
