777acdd731e093df56078e8a75ba9c537c4ce5c9
Benjamin Davies <bentendavies@gmail.com>
2025-03-01 15:35:40 +1300

    fix: cap update interval

--- blocks-game/src/lib.rs
+++ blocks-game/src/lib.rs
@@ -4,6 +4,8 @@ pub mod bounding_box;
 pub mod player;
 pub mod terrain;
 
+const MAX_DELTA_TIME: f32 = 0.05;
+
 pub struct Game {
     pub player: Player,
     pub terrain: Terrain,
@@ -18,6 +20,8 @@ impl Game {
     }
 
     pub fn update(&mut self, delta_time: f32) {
+        let delta_time = delta_time.min(MAX_DELTA_TIME);
+
         self.player.update(delta_time);
         self.player.collide_with_terrain(&self.terrain);
     }
